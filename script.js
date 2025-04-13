9c54e5e19e49dea8aa5af1fabe742caa97fc2ee8
Become_Your_Future <traji20071@gmx.fr>
2025-04-13 15:09:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   menu.classList.toggle('active');
 });
+
+// Script pour le menu mobile
+const mobileMenu = document.getElementById('mobile-menu');
+const menu = document.querySelector('.menu');
+
+mobileMenu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
