14e869d6fa26934ad39565261512cf339d35f6d6
Traji2007 <traji2007@laposte.net>
2025-04-14 21:46:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const menu = document.querySelector('.menu');
 mobileMenu.addEventListener('click', () => {
   menu.classList.toggle('active');
 })
+
+function isMobileDevice() {
+  return window.innerWidth <= 768; // seuil à ajuster selon vos besoins
+}
+
+if (!isMobileDevice()) {
+  // L'utilisateur est sur un ordinateur
+  document.querySelector('.rotate-message').style.display = 'none';
+  document.querySelector('.main-content').style.display = 'block';
+}
