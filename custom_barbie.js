ef00fcd589278d08cac72041219e1952142d2c12
Traji2007 <traji2007@laposte.net>
2025-04-14 21:00:24 +0200

    Create custom_barbie.js

---
+++ custom_barbie.js
@@ -0,0 +1,45 @@
+const text = "Hey !! Do you really think that we would have made that function available !! You're crazy !! We have way better... Take a look at this :";
+let index = 0;
+const speed = 50; // Vitesse de frappe en millisecondes
+
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("typed-text").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  } else {
+    // Afficher le bouton magique après la fin du texte
+    const magicButton = document.getElementById("magic-button");
+    magicButton.classList.remove("hidden");
+    magicButton.classList.add("fade-in");
+  }
+}
+document.getElementById('magic-button').addEventListener('click', function () {
+  const video = document.getElementById('fullscreen-video');
+  video.style.display = 'block'; // Affiche la vidéo
+  video.play(); // Lance la lecture
+
+  // Demande le mode plein écran
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) { /* Safari */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) { /* IE11 */
+    video.msRequestFullscreen();
+  }
+});
+
+// Écoute l'événement de changement de mode plein écran
+document.addEventListener('fullscreenchange', function () {
+  const video = document.getElementById('fullscreen-video');
+  if (!document.fullscreenElement) {
+    // Si le mode plein écran est désactivé, masque la vidéo
+    video.style.display = 'none';
+    video.pause(); // Met la lecture en pause
+    video.currentTime = 0; // Réinitialise la lecture au début
+  }
+});
+
+
+
+window.onload = typeWriter;
